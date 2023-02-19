52a6c17dbea0d24b061a67b145b3342451aa7d17
Imran <imranmansuri038@gmail.com>
2023-02-20 00:17:00 +0530

    navbar edited

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,56 +2,28 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+  const navLinkClass = ({ isActive }) => `
+    w-full text-base text-center font-nunito m-2.5
+    ${
+      isActive
+        ? "bg-cyan text-gray-300"
+        : "bg-gray-200 text-gray-100 hover:text-cyan active:bg-cyan active:text-gray-300"
+    }
+    border-0 cursor-pointer rounded capitalize font-semibold
+  `;
+
   return (
-    <nav
-      className="w-[40%] mt-16 flex justify-around align-middle
-  border border-cyan rounded-lg
-  "
-    >
-      <NavLink
-        to="/"
-        end
-        className={({ isActive }) => {
-          return `w-full text-base text-center font-nunito m-2.5
-${
-  isActive
-    ? "bg-cyan text-gray-300"
-    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
-}
-  border-0 cursor-pointer rounded capitalize font-semibold`;
-        }}
-      >
+    <nav className="w-[40%] mt-16 flex justify-around align-middle border border-cyan rounded-lg">
+      <NavLink to="/" end className={navLinkClass}>
         Crypto
       </NavLink>
 
-      <NavLink
-        to="/trending"
-        className={({ isActive }) => {
-          return `w-full text-base text-center font-nunito m-2.5
-${
-  isActive
-    ? "bg-cyan text-gray-300"
-    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
-}
-  border-0 cursor-pointer rounded capitalize font-semibold`;
-        }}
-      >
-        trending
+      <NavLink to="/trending" className={navLinkClass}>
+        Trending
       </NavLink>
 
-      <NavLink
-        to="/saved"
-        className={({ isActive }) => {
-          return `w-full text-base text-center font-nunito m-2.5
-${
-  isActive
-    ? "bg-cyan text-gray-300"
-    : "bg-gray-200 text-gray-100hover:text-cyan active:bg-cyan active:text-gray-300"
-}
-  border-0 cursor-pointer rounded capitalize font-semibold`;
-        }}
-      >
-        saved
+      <NavLink to="/saved" className={navLinkClass}>
+        Saved
       </NavLink>
     </nav>
   );
